d3aff8954c025b0e3951411fdb565ab5e2204d7b
Kiprotich-Code <fleeklawi@gmail.com>
2024-11-20 19:44:38 +0300

    error fix

--- cashnow/settings.py
+++ cashnow/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_&y5fpv#-=den=bjn_v&)d33-t1y7@-y2vzft56yn3ybw4()@7'
+SECRET_KEY = 'django-insecure-i*cauze%raxi4&i=d0%3#p48uxar7@qco0+&0b(drz5ee+$bqc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
